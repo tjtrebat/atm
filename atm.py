ad4cb0e4297b595acc100eb583c18f40aedb6305
Tom Trebat <tjtrebat@gmail.com>
2011-01-26 07:16:51 -0500

    started working on GUI

--- atm.py
+++ atm.py
@@ -8,12 +8,29 @@ class ATM:
         self.frame.pack()
         frame = Frame(self.frame, bd=1, relief=GROOVE)
         frame.grid()
-        Label(frame, text="Balance is xxx", height=2, width=20, anchor="nw").grid()
-        frame = Frame(self.frame, bd=1, relief=SUNKEN)
+        self.tv_balance = ""
+        self.lbl_balance = Label(frame, textvariable=self.tv_balance, height=2, width=21, anchor="nw")
+        self.lbl_balance.grid()
+        frame = Frame(self.frame, bd=2, relief=SUNKEN)
         frame.grid()
+        number_pad = Frame(frame, padx=20)
+        number_pad.grid(row=0, column=0)
         for i in range(9):
-            Button(frame, text=(i + 1), command= lambda x = i:self.num_click_handler(x)).grid(row=(i / 3), column=(i % 3))
-        Button(frame, text=0).grid(row=3, column=1)
+            Button(number_pad, text=(i + 1), command= lambda x = (i + 1):self.num_click_handler(x)).grid(row=(i / 3), column=(i % 3))
+        Button(number_pad, text=0).grid(row=3, column=1)
+        button_pad = Frame(frame)
+        button_pad.grid(row=0, column=1)
+        self.btn_ok = Button(button_pad, width=7, text="OK", state=DISABLED)
+        self.btn_ok.grid()
+        self.btn_balance = Button(button_pad, width=7, text="Balance")
+        self.btn_balance.grid()
+        self.btn_withdraw = Button(button_pad, width=7, text="Withdraw")
+        self.btn_withdraw.grid()
+        Button(button_pad, width=7, text="Done", command=quit).grid()
+
+    def num_click_handler(self, num):
+        pass
+        
 
 root = Tk()
 atm = ATM(root)
